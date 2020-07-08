646abaea3c853b175f163334c9c94ff228a1eeee
Ankur Heble <=>
2020-07-08 15:17:37 +0530

    Updates

---
+++ precache-manifest.1e193d9a7e8ca4a26dc9f91818303feb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa8ab598f115faf2f5c106afed86f59",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "ca02d6cbaf9ad88f0777",
+    "url": "/game-of-life/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3471929c53e0ade34dae",
+    "url": "/game-of-life/static/js/2.42529aa5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life/static/js/2.42529aa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca02d6cbaf9ad88f0777",
+    "url": "/game-of-life/static/js/main.38429cbe.chunk.js"
+  },
+  {
+    "revision": "a6893dc5f1659096e143",
+    "url": "/game-of-life/static/js/runtime-main.1dafd470.js"
+  }
+]);
